ecceb73dedae1c0cf951b087adef7ff2a384cccd
Balaji Wanave <bwanave>
2022-05-21 06:06:10 +0000

    Initial commit

---
+++ src/main/java/com/management/car/Application.java
@@ -0,0 +1,10 @@
+package com.management.car;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
